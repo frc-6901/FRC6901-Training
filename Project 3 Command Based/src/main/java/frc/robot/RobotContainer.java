a91018b3883ebc7e47d86739944152704144d1ce
Kumar-Samyak <113846541+Kumar-Samyak@users.noreply.github.com>
2022-11-12 17:34:22 -0600

    used Command Based Programming to  implement Project 2

--- Project 3 Command Based/src/main/java/frc/robot/RobotContainer.java
+++ Project 3 Command Based/src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.RomiDrivetrain;
+import frc.robot.subsystems.RomiDrivetrain.RomiState;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,6 +24,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final RomiDrivetrain m_romiDrivetrain = new RomiDrivetrain();
+  private int controllerPort = 0;
+  private final XboxController controller = new XboxController(controllerPort);
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_romiDrivetrain);
 
@@ -26,6 +33,17 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    // Configure default commands
+    // Set the default drive command to split-stick arcade drive
+    m_romiDrivetrain.setDefaultCommand(
+        // A split-stick arcade command, with forward/backward controlled by the left
+        // hand, and turning controlled by the right.
+        new RunCommand(
+            () ->
+                m_romiDrivetrain.arcadeDrive(
+                    -controller.getX(Hand.kLeft), -1 * controller.getY(Hand.kRight)),
+            m_romiDrivetrain));
   }
 
   /**
@@ -34,7 +52,27 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+    new JoystickButton(controller, Button.kX.value)
+    .whenPressed(
+                () -> {
+                  m_romiDrivetrain.setState(RomiState.SLOW);
+                }, m_romiDrivetrain);
+    
+    new JoystickButton(controller, Button.kY.value)
+    .whenPressed(
+                () -> {
+                  m_romiDrivetrain.setState(RomiState.NORMAL);
+                }, m_romiDrivetrain);
+
+    new JoystickButton(controller, Button.kB.value)
+    .whenPressed(
+                () -> {
+                  m_romiDrivetrain.setState(RomiState.FAST);
+                }, m_romiDrivetrain);
+
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
